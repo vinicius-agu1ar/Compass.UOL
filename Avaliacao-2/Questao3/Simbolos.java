b8231252b27e76f00b896146f0b5da0d167e7f74
Vinicius Aguiar <114755681+vinicius-agu1ar@users.noreply.github.com>
2022-10-30 17:08:17 -0300

    Add files via upload

---
+++ Avaliacao-2/Questao3/Simbolos.java
@@ -0,0 +1,30 @@
+package Questao3;
+
+public class Simbolos {
+
+    public String frase;
+    public String emoticon;
+    public int comparar;
+    public int contador;
+
+
+    public int contadorSimbolos(String frase, String emoticon)
+    {
+        int comparar = 0;
+        int contador = 0;
+        while (true)
+        {
+            comparar = frase.indexOf(emoticon, comparar);
+            if (comparar != -1)
+            {
+                contador ++;
+                comparar += emoticon.length();
+            }
+            else {
+                break;
+            }
+        }
+ 
+        return contador;
+    }
+}
